78959c2735791a8012702526b8f1082615a99ef3
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-02-03 19:44:13 +1300

    improve test

--- test/test.spec.js
+++ test/test.spec.js
@@ -2,8 +2,11 @@ import {Tester} from "../dist/index.js";
 
 const r = new Tester();
 
-r.test('eeee',() => {
-  console.log("test");
+r.test('success',() => {
   r.expectToBe(1, 1);
   r.expectTypeOf(1, "number");
+}); 
+
+r.test('fail',() => {
+  r.expectTypeOf(1, "string");
 }); 
